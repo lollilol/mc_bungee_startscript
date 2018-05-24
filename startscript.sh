653669cdc80d4163b635d258ab86e7e2f52d93fb
lollilol <38194372+lollilol@users.noreply.github.com>
2018-05-24 14:13:02 +0200

    made the script working
    now i've fixed the bugs, the script is working fine.

--- startscript.sh
+++ startscript.sh
@@ -8,10 +8,10 @@ mc3=/home/minecraft/survival
 
 ##screen
 #names
-bungee_screen=bungeecord
-mc1_screen=lobby
-mc2_screen=bedwars
-mc3_screen=survival
+bungee_name=bungeecord
+mc1_name=lobby
+mc2_name=bedwars
+mc3_name=survival
 #RAM
 # usage 
 # 1 Gigabyte, write: 1G
@@ -22,34 +22,33 @@ mc3_screen=survival
 
 ram_server1=1G
 ram_server2=1G
-ram_server3=1G
+ram_server3=2G
 
 #no changes beyond this line
 
 name=`basename "$0"`
-
 case "$1" in
 start)
-	cd $bungee && screen -AmdS $bungee_screen java -jar BungeeCord.jar
-	cd $mc1 && screen -AmdS $mc1_screen java -Xms$ram -Xmx$ram_server1 -jar minecraft_server.jar nogui
-	cd $mc2 && screen -AmdS $mc2_screen java -Xms$ram -Xmx$ram_server2 -jar minecraft_server.jar nogui
-	cd $mc3 && screen -AmdS $mc3_screen java -Xms$ram -Xmx$ram_server3 -jar minecraft_server.jar nogui
+	cd $bungee && screen -AmdS $bungee_name java -jar BungeeCord.jar
+	cd $mc1 && screen -AmdS $mc1_name java -Xms$ram_server1 -Xmx$ram_server1 -jar minecraft_server.jar nogui
+	cd $mc2 && screen -AmdS $mc2_name java -Xms$ram_server2 -Xmx$ram_server2 -jar minecraft_server.jar nogui
+	cd $mc3 && screen -AmdS $mc3_name java -Xms$ram_server3 -Xmx$ram_server3 -jar minecraft_server.jar nogui
 	sleep 1
 	echo "\033[32mBungeeCord and all Minecraft-Servers were started..\033[0m"
 	;;
 stop)
-	screen -S $bungee_screen -p 0 -X stuff "end^M"
-	screen -S $mc1_screen -p 0 -X stuff "stop^M"
-	screen -S $mc2_screen -p 0 -X stuff "stop^M"
-	screen -S $mc3_screen -p 0 -X stuff "stop^M"
+	screen -S $bungee_name -p 0 -X stuff "end^M"
+	screen -S $mc1_name -p 0 -X stuff "stop^M"
+	screen -S $mc2_name -p 0 -X stuff "stop^M"
+	screen -S $mc3_name -p 0 -X stuff "stop^M"
 	sleep 1
 	echo "\033[31mBungeeCord and all Minecraft-Servers were stopped..\033[0m"
 	;;
 reload)
-	screen -S $bungee_screen -p 0 -X stuff "greload^M"
-	screen -S $mc1_screen -p 0 -X stuff "reload^M"
-	screen -S $mc2_screen -p 0 -X stuff "reload^M"
-	screen -S $mc3_screen -p 0 -X stuff "reload^M"
+	screen -S $bungee_name -p 0 -X stuff "greload^M"
+	screen -S $mc1_name -p 0 -X stuff "reload^M"
+	screen -S $mc2_name -p 0 -X stuff "reload^M"
+	screen -S $mc3_name -p 0 -X stuff "reload^M"
 	sleep 1
 	echo "\033[31mBungeeCord and all Minecraft-Servers were reloaded..\033[0m"
 	;;
@@ -59,12 +58,12 @@ restart)
 	;;
 info)
 	echo "\033[32m$bungee_screen\033[0m is in: \033[32m$bungee\033[0m"
-	echo "\033[32m$mc1_screen\033[0m server is in: \033[32m$mc1\033[0m and has \033[32m$ram_server1\033[0m Ram."
-	echo "\033[32m$mc2_screen\033[0m server is in: \033[32m$mc2\033[0m and has \033[32m$ram_server2\033[0m Ram."
-	echo "\033[32m$mc3_screen\033[0m server is in: \033[32m$mc3\033[0m and has \033[32m$ram_server3\033[0m Ram."
+	echo "\033[32m$mc1_screen\033[0m server has \033[32m$ram_server1\033[0m Ram. [\033[32m$mc1\033[0m]"
+	echo "\033[32m$mc2_screen\033[0m sserver has \033[32m$ram_server2\033[0m Ram. [\033[32m$mc2\033[0m]"
+	echo "\033[32m$mc3_screen\033[0m server has \033[32m$ram_server3\033[0m Ram. [\033[32m$mc3\033[0m]"
 	;;
 *)
-	echo "\033[31mUsage: './$name (start|stop|restart|reload|info)'\033[0m"
+	echo "\033[31mUsage: '$name (start|stop|restart|reload|info)'\033[0m"
 	;;
 esac
 exit 0
